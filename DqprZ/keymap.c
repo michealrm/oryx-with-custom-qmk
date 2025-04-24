754907f21f2244c36f0555f8ac50fe0f4935cc8e
Micheal <8922961+michealrm@users.noreply.github.com>
2025-04-24 14:41:49 -0500

    change

--- DqprZ/keymap.c
+++ DqprZ/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TD(DANCE_0),    OSL(4),         KC_LEFT_CTRL,   KC_LEFT_ALT,    KC_LEFT_GUI,    KC_TAB,                                         LSFT(KC_TAB),   KC_RIGHT_GUI,   KC_RIGHT_ALT,   KC_RIGHT_CTRL,  KC_TRANSPARENT, TG(3),          
     KC_MINUS,       KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_QUOTE,       
     KC_LEFT_SHIFT,  KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_RIGHT_SHIFT, 
-    KC_CAPS,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_BSLS,        
+    QK_REP,         KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_BSLS,        
                                                     MEH_T(KC_SPACE),TG(2),                                          LT(5,KC_ENTER), KC_BSPC
   ),
   [1] = LAYOUT_voyager(
